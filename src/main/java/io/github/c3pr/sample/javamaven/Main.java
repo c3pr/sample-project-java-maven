5c7bf94769e5123cf0bc73b14a80352ea305ffd6
Antonio acdc Jr <acdcjunior@gmail.com>
2017-12-21 14:11:44 -0200

    feat(Main): add sample Main class with minor issues

---
+++ src/main/java/io/github/c3pr/sample/javamaven/Main.java
@@ -0,0 +1,12 @@
+package io.github.c3pr.sample.javamaven;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String someString = args[0];
+        if (someString.equals("a string at the right-hand side")) {
+            System.out.println("Equals...");
+        } else System.out.println("Else without braces");
+    }
+
+}
