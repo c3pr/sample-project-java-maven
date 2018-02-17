ade1bd2951c3256ffb329a087bc2337b3775d485
Antonio acdc Jr <acdcjunior@gmail.com>
2018-02-17 21:26:20 -0200

    chore: trigger webhook to test zeit agent

--- src/main/java/io/github/c3pr/sample/javamaven/Main.java
+++ src/main/java/io/github/c3pr/sample/javamaven/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         String someString = args[0];
+        // trigger
         if (someString.equals("a string at the right-hand side")) {
             System.out.println("Equals...");
         } else System.out.println("Else without braces");
