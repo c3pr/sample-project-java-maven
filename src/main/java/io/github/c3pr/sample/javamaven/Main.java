0f693794900afd8e82cad02022218f36b5df3a24
Antonio acdc Jr <acdcjunior@gmail.com>
2018-03-03 10:26:04 -0300

    chore: cleanup demo

--- src/main/java/io/github/c3pr/sample/javamaven/Main.java
+++ src/main/java/io/github/c3pr/sample/javamaven/Main.java
@@ -3,10 +3,7 @@ package io.github.c3pr.sample.javamaven;
 public class Main {
 
     public static void main(String[] args) {
-        String someString = args[0];
-        if (someString.equals("a string at the right-hand side")) {
-            System.out.println("Equals...");
-        } else System.out.println("Else without braces");
+        System.out.println("Hello C-3PR");
     }
 
 }
