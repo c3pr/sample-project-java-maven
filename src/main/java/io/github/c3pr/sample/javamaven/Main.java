b6f31de3616f69ef1cd7928b8a7e53dc1eb50ce9
Antonio acdc Jr <acdcjunior@gmail.com>
2018-03-03 11:18:36 -0300

    feat: disparar job

--- src/main/java/io/github/c3pr/sample/javamaven/Main.java
+++ src/main/java/io/github/c3pr/sample/javamaven/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello C-3PR");
+        String primeiroArg = args[0];
+        if (primeiroArg.equals("algo") {
+            // comando
+        }
     }
 
 }
