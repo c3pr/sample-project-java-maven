57fc4215c9ee54438a5ee9fde851dd2c0595f664
Antonio Jr <acdcjunior@gmail.com>
2018-03-28 14:00:36 -0300

    fix: fix compile error and add import to test new rule

--- src/main/java/io/github/c3pr/sample/javamaven/Main.java
+++ src/main/java/io/github/c3pr/sample/javamaven/Main.java
@@ -1,11 +1,13 @@
 package io.github.c3pr.sample.javamaven;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello C-3PR");
         String primeiroArg = args[0];
-        if (primeiroArg.equals("algo") {
+        if (primeiroArg.equals("algo")) {
             // comando
         }
     }
