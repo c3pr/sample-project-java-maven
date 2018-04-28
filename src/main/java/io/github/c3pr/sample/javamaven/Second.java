8da2ba03b40a115a80fc4bc30f9d58085211dce5
Antonio acdc Jr <acdcjunior@gmail.com>
2018-04-27 23:48:38 -0300

    feat: add second file

---
+++ src/main/java/io/github/c3pr/sample/javamaven/Second.java
@@ -0,0 +1,19 @@
+package io.github.c3pr.sample.javamaven;
+
+public class Second {
+
+    public static void main(String[] args) {
+        System.out.println("Hello C-3PR");
+        String firstArg = args[0];
+        if (firstArg.equals("algo")) {
+            // command
+        }
+
+        String x = "a";
+        String b = args[0];
+        if (x == b) {
+            System.out.println("oyo");
+        }
+    }
+
+}
