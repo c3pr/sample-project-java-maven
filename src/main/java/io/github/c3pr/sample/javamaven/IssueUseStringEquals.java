faf08870b2408728fd130a5e1870f770796c6fd7
Antonio acdc Jr <acdcjunior@gmail.com>
2018-05-04 12:50:54 -0300

    feat(UseStringEquals): make code sample clearer

--- src/main/java/io/github/c3pr/sample/javamaven/IssueUseStringEquals.java
+++ src/main/java/io/github/c3pr/sample/javamaven/IssueUseStringEquals.java
@@ -3,32 +3,29 @@ package io.github.c3pr.sample.javamaven;
 public class IssueUseStringEquals {
 
     public static void main(String[] args) {
-        String str1 = "blue";
-        String str2 = new String("blue");
-        String str3 = str1;
+        String strOne = "blue";
+        String strTwo = new String("blue");
 
 
         //region COVERED BY CURRENT sonar:UseStringEquals
-        if (str1 == "blue") {
-            System.out.println("they're both 'blue' 2"); // this prints
+        if (strOne == "blue") {
+            System.out.println("strOne is \"blue\""); // prints
         }
-        if (str2 == "blue") {
-            System.out.println("they're both 'blue' 3"); // this doesn't print because the objects are different
+        if (strTwo == "blue") {
+            System.out.println("strTwo is \"blue\""); // DOESN'T print
         }
-        if (str1 != "blue") {
-            System.out.println("they're both 'blue' 2"); // this prints
+        if (strOne != "blue") {
+            System.out.println("strOne is not \"blue\""); // prints
         }
-        if (str2 != "blue") {
-            System.out.println("they're both 'blue' 3"); // this doesn't print because the objects are different
+        if (strTwo != "blue") {
+            System.out.println("strTwo is not \"blue\""); // DOESN'T print
         }
         //endregion
 
         //region NOT covered by current sonar:UseStringEquals
-        if (str1 == str2) {
-            System.out.println("they're both 'blue' 1"); // this doesn't print because the objects are different
-        }
-        if (str1 == str3) {
-            System.out.println("they're the same object"); // this prints
+        String strThree = strOne;
+        if (strOne == strTwo) {
+            System.out.println("comparing two string references, not one string reference and one literal");
         }
         //endregion
     }
