c5f5f7c51ad9c6cd8b73ea85896257014a00c593
Antonio acdc Jr <acdcjunior@gmail.com>
2018-05-04 12:36:32 -0300

    feat: add string equality example

---
+++ src/main/java/io/github/c3pr/sample/javamaven/IssueUseStringEquals.java
@@ -0,0 +1,24 @@
+package io.github.c3pr.sample.javamaven;
+
+public class IssueUseStringEquals {
+
+    public static void main(String[] args) {
+        String str1 = "blue";
+        String str2 = new String("blue");
+        String str3 = str1;
+
+        if (str1 == str2) {
+            System.out.println("they're both 'blue' 1"); // this doesn't print because the objects are different
+        }
+        if (str1 == "blue") {
+            System.out.println("they're both 'blue' 2"); // this prints
+        }
+        if (str2 == "blue") {
+            System.out.println("they're both 'blue' 3"); // this doesn't print because the objects are different
+        }
+        if (str1 == str3) {
+            System.out.println("they're the same object"); // this prints
+        }
+    }
+
+}
