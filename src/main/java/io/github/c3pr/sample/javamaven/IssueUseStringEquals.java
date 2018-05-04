7394d086c8ed8d6ec8ba4e820d8073daaf24e9fc
Antonio acdc Jr <acdcjunior@gmail.com>
2018-05-04 12:41:09 -0300

    feat(UseStringEquals): make supported/unsupported clear

--- src/main/java/io/github/c3pr/sample/javamaven/IssueUseStringEquals.java
+++ src/main/java/io/github/c3pr/sample/javamaven/IssueUseStringEquals.java
@@ -7,18 +7,30 @@ public class IssueUseStringEquals {
         String str2 = new String("blue");
         String str3 = str1;
 
-        if (str1 == str2) {
-            System.out.println("they're both 'blue' 1"); // this doesn't print because the objects are different
-        }
+
+        //region COVERED BY CURRENT sonar:UseStringEquals
         if (str1 == "blue") {
             System.out.println("they're both 'blue' 2"); // this prints
         }
         if (str2 == "blue") {
             System.out.println("they're both 'blue' 3"); // this doesn't print because the objects are different
         }
+        //endregion
+
+        //region NOT covered by current sonar:UseStringEquals
+        if (str1 != "blue") {
+            System.out.println("they're both 'blue' 2"); // this prints
+        }
+        if (str2 != "blue") {
+            System.out.println("they're both 'blue' 3"); // this doesn't print because the objects are different
+        }
+        if (str1 == str2) {
+            System.out.println("they're both 'blue' 1"); // this doesn't print because the objects are different
+        }
         if (str1 == str3) {
             System.out.println("they're the same object"); // this prints
         }
+        //endregion
     }
 
 }
