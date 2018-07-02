6356c276546ab97585670310b36ae3e49c8b6a1e
Antonio acdc Jr <acdcjunior@gmail.com>
2018-07-02 19:03:47 -0300

    refactor: improve naming

--- src/main/java/io/github/c3pr/sample/javamaven/walkmod/StaticInitializedFieldToFinal.java
+++ src/main/java/io/github/c3pr/sample/javamaven/walkmod/StaticInitializedFieldToFinal.java
@@ -2,11 +2,10 @@ package io.github.c3pr.sample.javamaven.walkmod;
 
 public class StaticInitializedFieldToFinal {
 
-    public static String bobNelson = "Bob Nelson";
+    public static String thisVariableWillBeFinal = "Some Value";
 
     public String something() {
-        bobNelson += "bobz";
-        return "" + bobNelson;
+        return "" + thisVariableWillBeFinal;
     }
 
 }
