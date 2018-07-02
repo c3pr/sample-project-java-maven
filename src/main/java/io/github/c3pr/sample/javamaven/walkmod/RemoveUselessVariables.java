313acf2f26aea88285272f8a2bb2d56fe614355b
Antonio acdc Jr <acdcjunior@gmail.com>
2018-07-02 19:37:51 -0300

    feat: improve RemoveUselessVariables

--- src/main/java/io/github/c3pr/sample/javamaven/walkmod/RemoveUselessVariables.java
+++ src/main/java/io/github/c3pr/sample/javamaven/walkmod/RemoveUselessVariables.java
@@ -1,10 +1,12 @@
 package io.github.c3pr.sample.javamaven.walkmod;
 
+import io.github.c3pr.sample.javamaven.walkmod.support.Support;
+
 public class RemoveUselessVariables {
 
-    public String removeUselessVariables() {
+    public String removeUselessVariables(Support support) {
         String uselessVariableOne = "value";
-        String uselessVariableTwo = "value";
+        String uselessVariableTwo = support.someStringMethod();
         String uselessVariableThree = "value";
 
         return "" + uselessVariableOne;
---
+++ src/main/java/io/github/c3pr/sample/javamaven/walkmod/support/Support.java
@@ -0,0 +1,7 @@
+package io.github.c3pr.sample.javamaven.walkmod.support;
+
+public interface Support {
+
+    String someStringMethod();
+
+}
