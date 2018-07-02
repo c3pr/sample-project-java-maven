67925059764711cc40b7180023fb1335a16996ee
Antonio acdc Jr <acdcjunior@gmail.com>
2018-07-02 19:04:58 -0300

    feat: RemoveUselessVariables

---
+++ src/main/java/io/github/c3pr/sample/javamaven/walkmod/RemoveUselessVariables.java
@@ -0,0 +1,13 @@
+package io.github.c3pr.sample.javamaven.walkmod;
+
+public class RemoveUselessVariables {
+
+    public String removeUselessVariables() {
+        String uselessVariableOne = "value";
+        String uselessVariableTwo = "value";
+        String uselessVariableThree = "value";
+
+        return "" + uselessVariableOne;
+    }
+
+}
