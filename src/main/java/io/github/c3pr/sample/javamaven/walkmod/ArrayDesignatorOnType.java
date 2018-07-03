802836dece83b10eeb9ac8920ef7acf329332cd9
Antonio Jr <acdcjunior@gmail.com>
2018-07-02 22:49:15 -0300

    feat: add ArrayDesignatorOnType

---
+++ src/main/java/io/github/c3pr/sample/javamaven/walkmod/ArrayDesignatorOnType.java
@@ -0,0 +1,11 @@
+package io.github.c3pr.sample.javamaven.walkmod;
+
+public class ArrayDesignatorOnType {
+
+    public String arrayDesignatorOnType(String yay) {
+        int matrix[][];
+
+        return "" + yay;
+    }
+
+}
